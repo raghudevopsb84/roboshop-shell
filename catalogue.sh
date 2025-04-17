57f275ebd565d206ac438c5a1ce10bd5a7b9887f
Raghu K <raghuk.vit@gmail.com>
2025-04-17 07:01:39 +0530

    RoboShop Project

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,9 @@
 component_name=catalogue
 source common.sh
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-
 nodejs
 
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.rdevopsb84.online </app/db/master-data.js
 
