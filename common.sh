8270cc4bcab5be9ebc195ebdb9a1d224a0ad4bf2
Raghu K <raghuk.vit@gmail.com>
2025-04-17 07:11:52 +0530

    RoboShop Project

--- common.sh
+++ common.sh
@@ -1,39 +1,33 @@
+pwd=$(pwd)
+
+
 systemd_setup() {
+  cp ${pwd}/${component_name}.service /etc/systemd/system/${component_name}.service
   systemctl daemon-reload
   systemctl enable ${component_name}
   systemctl start ${component_name}
 }
 
-nodejs() {
-  cp ${component_name}.service /etc/systemd/system/${component_name}.service
-  dnf module disable nodejs -y
-  dnf module enable nodejs:20 -y
-  dnf install nodejs -y
+app_pre_setup() {
   useradd roboshop
   rm -rf /app
   mkdir /app
   cd /app
   npm install
   curl -L -o /tmp/${component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_name}-v3.zip
-  cd /app
   unzip /tmp/${component_name}.zip
-  cd /app
+}
+
+nodejs() {
+  dnf module disable nodejs -y
+  dnf module enable nodejs:20 -y
+  dnf install nodejs -y
+  app_pre_setup
   npm install
 }
 
-pwd=$(pwd)
-
 python() {
   dnf install python3 gcc python3-devel -y
-
-  cp payment.service /etc/systemd/system/payment.service
-  useradd roboshop
-  rm -rf /app
-  mkdir /app
-
-  curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
-  cd /app
-  unzip /tmp/payment.zip
-  cd /app
+  app_pre_setup
   pip3 install -r requirements.txt
 }
