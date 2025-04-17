7e26d9f62b49a1c0406d729203bd649c00eb0876
Raghu K <raghuk.vit@gmail.com>
2025-04-17 07:00:38 +0530

    RoboShop Project

--- cart.sh
+++ cart.sh
@@ -1,20 +1,9 @@
 component_name=cart
 source common.sh
 
+
 nodejs
 
-cp cart.service /etc/systemd/system/cart.service
-
-useradd roboshop
-rm -rf /app
-mkdir /app
-cd /app
-npm install
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-cd /app
-unzip /tmp/cart.zip
-cd /app
-npm install
 
 systemd_setup
 
--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,9 @@
 component_name=catalogue
 source common.sh
 
-nodejs
-
-cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
+nodejs
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.rdevopsb84.online </app/db/master-data.js
