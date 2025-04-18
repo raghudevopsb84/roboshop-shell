6fe0b6b707d7bdd559a22110287a6eacabfbc236
Raghu K <raghuk.vit@gmail.com>
2025-04-18 06:36:43 +0530

    RoboShop Project

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,19 @@
-dnf install mysql-server -y
+if [ -z "${MYSQL_ROOT_PASSWORD}" -a -z "$1" ]; then
+  echo Input password is missing
+  exit
+fi
 
+if [ -n "${MYSQL_ROOT_PASSWORD}" ]; then
+  PASSWORD=$MYSQL_ROOT_PASSWORD
+fi
+
+if [ -n "$1" ];then
+  PASSWORD=$1
+fi
+
+dnf install mysql-server -y
 systemctl enable mysqld
 systemctl start mysqld
 
-mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD
+mysql_secure_installation --set-root-pass $PASSWORD
 
