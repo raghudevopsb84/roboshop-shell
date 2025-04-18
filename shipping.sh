6564c6743ccbdfac08ba87ca063eee2d94ff930a
Raghu K <raghuk.vit@gmail.com>
2025-04-18 06:54:04 +0530

    RoboShop Project

--- shipping.sh
+++ shipping.sh
@@ -1,11 +1,16 @@
+if [ -z "$1" ]; then
+  echo Input DB Password is missing
+  exit
+fi
+
 component_name=shipping
 source common.sh
 
 java
 
 dnf install mysql -y
-mysql -h mysql-dev.rdevopsb84.online -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.rdevopsb84.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.rdevopsb84.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+for file in schema app-user master-data; do
+  mysql -h mysql-dev.rdevopsb84.online -uroot -p$1 < /app/db/$file.sql
+done
 
 systemd_setup
