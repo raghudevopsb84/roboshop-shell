9cfeeca1c1a26d54fb2715da6b32de8fbf8b9415
Raghu K <raghuk.vit@gmail.com>
2025-04-18 06:23:55 +0530

    RoboShop Project

--- mysql.sh
+++ mysql.sh
@@ -3,5 +3,5 @@ dnf install mysql-server -y
 systemctl enable mysqld
 systemctl start mysqld
 
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD
 
